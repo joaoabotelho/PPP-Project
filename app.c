cc7fd19c5dd8ddf6641d69560eac131e6b23e101
João Botelho <joao.botelho@jeknowledge.com>
2017-05-16 18:13:48 +0100

    changes the design of the menu

--- app.c
+++ app.c
@@ -14,59 +14,94 @@
 void exams_menu(Student_list list_student, Classes_list list_class, Exams_list list_exam) {
     char choice[50];
 
-        printf("$ MENU $\t1.Crie um novo exame.\t2.Mostrar os exames.\n");
-        fgets(choice,50,stdin);
+    printf("\n\n$ EXAMS MENU $\n\t1.Crie um novo exame.\n\t2.Mostrar os exames.\n-->");
+    fgets(choice,50,stdin);
 
-        switch(choice[0]) {
+    switch(choice[0]) {
 
-            case '1': 
-                create_exam(list_exam, list_class);
-                break;
-            case '2':
-                print_exams_list(list_exam);
-                break;
-            default:
-                printf("Wrong choice. Enter again.\n");
-                exams_menu(list_student, list_class, list_exam);
-        }  
+        case '1': 
+            create_exam(list_exam, list_class);
+            break;
+        case '2':
+            print_exams_list(list_exam);
+            break;
+        default:
+            printf("Wrong choice. Enter again.\n");
+            exams_menu(list_student, list_class, list_exam);
+    }  
 }
 
+void classes_menu(Student_list list_student, Classes_list list_class, Exams_list list_exam) {
+    char choice[50];
+
+    printf("\n\n$ CLASSES MENU $\n");
+    printf("\t1.Crie uma numa disciplina.\n\t2.Mude os dados de uma disciplina.\n\t3.Mostrar as disciplinas existentes.\n\t4.Remover uma disciplina da base de dados.\n-->");
+    fgets(choice,50,stdin);
+
+    switch(choice[0]) {
+
+        case '1':
+            new_class(list_class);
+            break;
+        case '2':
+            change_clist_data(&list_class);
+            break;
+        case '3':
+            print_classes_list(list_class);
+            break;
+        case '4':
+            remove_class_data(list_class);
+            break;
+        default:
+            printf("Wrong choice. Enter again.\n");
+            classes_menu(list_student, list_class, list_exam);
+    }  
+}
+
+void students_menu(Student_list list_student, Classes_list list_class, Exams_list list_exam) {
+    char choice[50];
+
+    printf("\n\n$ STUDENTS MENU $\n");
+    printf("\t1.Crie um novo aluno.\n\t2.Mude os dados de um aluno.\n\t3.Mostrar lista de alunos existentes.\n\t4.Remover um aluno da base de dados.\n-->");
+    fgets(choice,50,stdin);
+
+    switch(choice[0]) {
+
+        case '1': 
+            new_student(list_student);
+            break;
+        case '2': 
+            change_slist_data(list_student);
+            break;
+        case '3':
+            print_student_list(list_student);
+            break;
+        case '4':
+            remove_student_data(list_student);
+            break;
+        default:
+            printf("Wrong choice. Enter again.\n");
+            students_menu(list_student, list_class, list_exam);
+    }
+}
 void main_menu(Student_list list_student, Classes_list list_class, Exams_list list_exam) {
     char choice[50];
 
     while(1) { 
         printf("@@@@@@      Welcome to this app      @@@@@@\n\n");
-        printf("$ MENU $\t1.Crie um novo perfil estudante.\t2.Mude os dados de um estudante.\t3.Mostrar a lista de estudantes.\t4.Remover um estudant da base de dados.\n");
-        printf("        \t5.Crie uma numa disciplina.\t6.Mude os dados de uma disciplina.\t7.Mostrar as disciplinas existentes.\t8.Remover uma disciplina da base de dados.\n");
-        printf("        \t9. Exams Menu\n-->");
+        printf("$ MENU $\t1. Students Menu\n");
+        printf("        \t2. Classes Menu\n"); 
+        printf("        \t3. Exams Menu\n-->");
         fgets(choice,50,stdin);
 
         switch(choice[0]) {
-            case '1': 
-                new_student(list_student);
+            case '1':
+                students_menu(list_student, list_class, list_exam);
                 break;
-            case '2': 
-                change_slist_data(list_student);
+            case '2':
+                classes_menu(list_student, list_class, list_exam);
                 break;
             case '3':
-                print_student_list(list_student);
-                break;
-            case '4':
-                remove_student_data(list_student);
-                break;
-            case '5':
-                new_class(list_class);
-                break;
-            case('6'):
-                change_clist_data(&list_class);
-                break;
-            case '7':
-                print_classes_list(list_class);
-                break;
-            case '8':
-                remove_class_data(list_class);
-                break;
-            case '9':
                 exams_menu(list_student, list_class, list_exam);
                 break;
             default:
@@ -83,6 +118,6 @@ int main() {
     Exams_list list_exam = create_exams_list();
 
     main_menu(list_student, list_class, list_exam);
-   
+
     return 0;
 }
